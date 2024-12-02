9d1607a0b8472712aaaafdd960a49adf2b012aec
Anderson Harry Robert Taylor <handerso@bb-qqfpgfkw57.epfl.ch>
2024-12-02 23:33:24 +0100

    fixed linter errors in test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,74 +1,64 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
-import React from "react";
-import { afterEach, vi } from "vitest";
-import * as formattedMockData from "./formatted-mock-data.json" assert { type: "json" };
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import * as formattedMockData from './formatted-mock-data.json';
 
-import App from "./App";
-import * as fetchReposModule from "./fetchRepos";
+import App from './App';
+import * as fetchReposModule from './fetchRepos';
 
 afterEach(() => {
   // restore the spy created with spyOn
   vi.restoreAllMocks();
 });
 
-describe("the repo fetcher UI", () => {
-  it("loads and displays a text input", async () => {
+describe('the repo fetcher UI', () => {
+  it('loads and displays a text input', async () => {
     render(<App />);
 
-    const input = screen.getByLabelText("Repository name");
+    const input = screen.getByLabelText('Repository name');
 
     expect(input).toBeInTheDocument();
   });
 
-  it("can fetch repositories from the API -- Mock implementation", async () => {
-    const spy = vi
-      .spyOn(fetchReposModule, "fetchRepos")
-      .mockResolvedValue(formattedMockData);
+  it('can fetch repositories from the API -- Mock implementation', async () => {
+    const spy = vi.spyOn(fetchReposModule, 'fetchRepos').mockResolvedValue(formattedMockData);
 
     const user = userEvent.setup();
 
     render(<App />);
 
-    const input = screen.getByLabelText("Repository name");
+    const input = screen.getByLabelText('Repository name');
 
-    await user.type(input, "sonar");
+    await user.type(input, 'sonar');
 
-    await waitFor(() =>
-      expect(spy).toHaveBeenCalledWith("sonar", expect.any(Function))
-    );
+    await waitFor(() => expect(spy).toHaveBeenCalledWith('sonar', expect.any(Function)));
   });
 
-  it("render a list of fetched repositories (with clickable links)", async () => {
-    const spy = vi
-      .spyOn(fetchReposModule, "fetchRepos")
-      .mockResolvedValue(formattedMockData);
+  it('render a list of fetched repositories (with clickable links)', async () => {
+    const spy = vi.spyOn(fetchReposModule, 'fetchRepos').mockResolvedValue(formattedMockData);
 
     const user = userEvent.setup();
 
     render(<App />);
 
-    const input = screen.getByLabelText("Repository name");
+    const input = screen.getByLabelText('Repository name');
 
-    await user.type(input, "sonar");
+    await user.type(input, 'sonar');
 
     await waitFor(() => {
       // Second param is a callback
-      expect(spy).toHaveBeenCalledWith("sonar", expect.any(Function));
+      expect(spy).toHaveBeenCalledWith('sonar', expect.any(Function));
     });
 
-    const repoLink = screen.getByRole("link", {
-      textContent: "SonarSource/sonarqube",
+    const repoLink = screen.getByRole('link', {
+      textContent: 'SonarSource/sonarqube',
     });
 
     expect(repoLink).toBeInTheDocument();
 
-    expect(repoLink).toHaveAttribute(
-      "href",
-      "https://github.com/SonarSource/sonarqube"
-    );
+    expect(repoLink).toHaveAttribute('href', 'https://github.com/SonarSource/sonarqube');
 
-    expect(repoLink).toHaveAttribute("target", "_blank");
+    expect(repoLink).toHaveAttribute('target', '_blank');
   });
 });
